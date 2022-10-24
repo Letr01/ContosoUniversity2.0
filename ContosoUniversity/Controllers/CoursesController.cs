11a90d5cb2ab1f9140b376cee3d751c2b79c53bb
opilane <opilane@FUJ-YMHP016313>
2022-10-24 09:46:45 +0300

    Add project files.

---
+++ ContosoUniversity/Controllers/CoursesController.cs
@@ -0,0 +1,32 @@
+﻿using ContosoUniversity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public CoursesController(
+            SchoolContext context
+           )
+        {
+            _context = context;
+
+        }
+        public async Task<IActionResult> Index()
+        {
+            var courses = await _context.Courses
+                .Include(c => c.Department)
+                .AsNoTracking()
+                .ToListAsync();
+ 
+            return View(courses);
+        }
+    }
+}
